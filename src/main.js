3d4683d4d55e4500c11f0c4142ea5753dac59ba6
Konano <nanoapezlk@gmail.com>
2025-11-12 13:20:09 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,3 +1,82 @@
-import { hello } from './utils.js';
+import { appendMetaId, dlc } from './utils.js';
+// import * as utils from './utils.js';
 
-hello('Tampermonkey');
+const main = (t) => {
+    checksBeforeRunning();
+    // setTestLogConsole();
+    // quitOnAdFrames()
+    //     .then(function () { return jqueryInit(t); })
+    //     .then(function () { return browserInit(t); })
+    //     .then(function () { return constInit(t); })
+    //     .then(function () { return variablesInit(t); })
+    //     .done(function () {
+    //         function checkBodyContent(waitCount) {
+    //             if ($('body').children().length > 1 || ($('body').children().length == 1 && document.location.href.match(/^https:\/\/www\.certitudes\.org\/(certify|certitude\?wp=[A-Z0-9]{1,15})/))) {
+    //                 tlc('BodyContent found');
+    //                 if (document.location.href.match(/^https:\/\/maps\.google\./) || document.location.href.match(/^https:\/\/www\.google\.[a-zA-Z.]*\/maps/)) {
+    //                     mainGMaps();
+    //                 } else if (document.location.href.match(/^https:\/\/www\.google\.[a-zA-Z.]*\/search/)) {
+    //                     mainGSearch();
+    //                 } else if (document.location.href.match(/^https:\/\/www\.openstreetmap\.org/)) {
+    //                     mainOSM();
+    //                 } else if (document.location.href.match(/^https:\/\/www\.geocaching\.com/)) {
+    //                     mainGCWait();
+    //                 } else if (document.location.href.match(/^https:\/\/project-gc\.com\/Tools\/PQSplit/)) {
+    //                     mainPGC();
+    //                 } else if (document.location.href.match(/^https:\/\/www\.certitudes\.org\/(certify|certitude\?wp=[A-Z0-9]{1,15})/)) {
+    //                     mainCertitudes();
+    //                 }
+    //             } else { waitCount++; if (waitCount <= 5000) setTimeout(function () { checkBodyContent(waitCount); }, 10); }
+    //         }
+    //         dlc('START checkBodyContent');
+    //         checkBodyContent(0);
+    //     });
+}
+
+// Checks before running the main function.
+const checksBeforeRunning = () => {
+  /*if (typeof GM.info != "undefined" && typeof GM.info.scriptHandler != "undefined" && GM.info.scriptHandler == 'Greasemonkey') {
+        var text = 'Sorry, the script GC little helper II does not run with script manager Greasemonkey. Please use the script manager Tampermonkey or an other similar script manager.\n\nDo you want to see the "Tips for the installation"?\n ';
+        var url = 'https://github.com/2Abendsegler/GClh/blob/master/docu/tips_installation.md#en';
+        if (window.confirm(text)) window.open(url, '_blank');
+        throw Error('Abort because of GClh II installation under script manager Greasemonkey.');
+    }
+    if (document.getElementsByTagName('head')[0] && document.getElementById('GClh_II_running')) {
+        var text = 'Sorry, the script GC little helper II is already running. Please make sure that it runs only once.\n\nDo you want to see tips on how this could happen and what you can do about it?';
+        var url = 'https://github.com/2Abendsegler/GClh/blob/master/docu/faq.md#1-en';
+        if (window.confirm(text)) window.open(url, '_blank');
+        throw Error('Abort because of GClh II already running.');
+    } else */ appendMetaId('GC_Assist_running');
+    // function checkForOldGClh(waitCount) {
+    //     var linklists = 0;
+    //     $('gclh_nav ul li a.Dropdown').each(function () { if ($(this)[0].innerHTML == 'Linklist') linklists++; });
+    //     if (linklists > 1) {
+    //         var text = 'Sorry, the script GC little helper II does not run together with the OLDER GC little helper (without the "II").\n\nYou have installed also the older GC little helper as script in your script manager or as add on in your browser. This older GC little helper has not been maintained since 2016 and works not correctly.\n\nPlease delete this older GC little helper.\n';
+    //         alert(text);
+    //         throw Error('Abort because of older GClh installation.');
+    //     } else { waitCount++; if (waitCount <= 20) setTimeout(function () { checkForOldGClh(waitCount); }, 500); }
+    // }
+    // checkForOldGClh(0);
+}
+
+// function setTestLogConsole() {
+//     test_log_console = GM_getValue('test_log_console', false);
+//     GM_setValue('test_log_console', test_log_console);
+// };
+
+// Quit on Ad Frames.
+const quitOnAdFrames = (t) => {
+    dlc('START quitOnAdFrames');
+    var quitOnAdFramesDeref = new jQuery.Deferred();
+    if (window.name) {
+        if (window.name.substring(0, 18) !== 'google_ads_iframe_')
+            quitOnAdFramesDeref.resolve();
+        else quitOnAdFramesDeref.reject();
+    } else {
+        quitOnAdFramesDeref.resolve();
+    }
+    return quitOnAdFramesDeref.promise();
+}
+
+main(globalThis);
+dlc('GCAssist main.js loaded.');
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
-export function hello(name) {
-    console.log(`Hello, ${name}!`);
+// Add meta info.
+export const appendMetaId = (id) => {
+    const head = document.head;
+    const meta = document.createElement('meta');
+    meta.id = id;
+    head.appendChild(meta);
+}
+
+// Debug log console.
+export const dlc = (output) => {
+    // TODO: use test_log_console to control logging
+    if (output?.trim()) console.info(`GCAssist: ${output}`);
 }
