0654e51d9e7075750e4ab9cc77497739acb642ad
Konano <nanoapezlk@gmail.com>
2025-11-13 10:35:36 +0800

    小修小改

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # GC Assistant
 
+A userscript to enhance the geocaching.com experience.
+
+## 目前实现了的功能
+
+-   浏览 cache 页面时，自动显示 log 中的图片缩略图，鼠标悬停可查看大图
+
 ## Build
 
 ```
--- geocaching-assist.user.js
+++ geocaching-assist.user.js
@@ -3,7 +3,7 @@
 // @description  A userscript to enhance the geocaching.com experience.
 // @author       Konano
 // @version      0.0.1
-// @license      GNU General Public License v2.0
+// @license      GNU General Public License v3.0
 // @supportURL   https://github.com/Konano/GC-Assist/issues
 // @namespace    https://github.com/Konano
 // @run-at       document-end
